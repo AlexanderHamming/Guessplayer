91b36a5cdab802eaf0c0f3690ba7031cef393dfc
Alexander Hamming <alexanderhamming17@gmail.com>
2024-05-14 21:55:33 +0200

    little more

--- src/main.ts
+++ src/main.ts
@@ -8,49 +8,49 @@ import FlagData from "./counter";
 const lattareSpelare: LattareSpelareRoot = lattaspelareData;
 const svarareSpelare: SvarareSpelareRoot = svararespelareData;
 
-const startpageEL = document.querySelector(".container") as HTMLElement
-const startBtnEasy = document.querySelector(".startBtnEasy") as HTMLElement;
-const startBtnHard = document.querySelector(".startBtnHard") as HTMLElement;
+const startpageEL = document.querySelector(".container") as HTMLElement;
+const startBtnEasy = document.querySelector(".startBtnEasy") as HTMLButtonElement;
+const startBtnHard = document.querySelector(".startBtnHard") as HTMLButtonElement;
 const playerListDiv = document.querySelector(".playerList") as HTMLElement; 
-const startedGameEL = document.querySelector(".startedGame") as HTMLElement
-const guessBtnEL = document.querySelector(".guessButton")
-const guessInputEL = document.querySelector(".guessInput") as HTMLInputElement;
+const startedGameEL = document.querySelector(".startedGame") as HTMLElement;
+const guessForm = document.querySelector("#guessForm") as HTMLFormElement;
+const guessInputEL = document.querySelector("#guessInput") as HTMLInputElement;
 
 const flagImages: FlagData = {
-    "England": "./flag_images/england.png",
-    "Danmark": "./flag_images/denmark.png",
-    "Italien": "./flag_images/italy.png",
-    "Frankrike": "./flag_images/france.png",
-    "Argentina": "./flag_images/argentina.png",
-    "Belgien": "./flag_images/belgium.png",
-    "Bosnien": "./flag_images/bosnia.png",
-    "Brasilien": "./flag_images/brazil.png",
-    "Chile": "./flag_images/chile.png",
-    "Kina": "./flag_images/china.png",
-    "Colombia": "./flag_images/colombia.png",
-    "Kroatien": "./flag_images/croatia.png",
-    "Tjeckien": "./flag_images/czechia.png",
-    "Tyskland": "./flag_images/germany.png",
-    "Grekland": "./flag_images/grecce.png",
-    "Irland": "./flag_images/ireland.png",
-    "Israel": "./flag_images/israel.png",
-    "Mexiko": "./flag_images/mexico.png",
-    "Montenegro": "./flag_images/montenegro.png",
-    "Nederländerna": "./flag_images/netherlands.png",
-    "Paraguay": "./flag_images/paraguay.png",
-    "Portugal": "./flag_images/portugal.png",
-    "Qatar": "./flag_images/qatar.png",
-    "Ryssland": "./flag_images/russia.png",
-    "Saudiarabien": "./flag_images/saudi_arabia.png",
-    "Skottland": "./flag_images/scotland.png",
-    "Serbien": "./flag_images/serbia.png",
-    "Slovenien": "./flag_images/slovenia.png",
-    "Sydafrika": "./flag_images/south_africa.png",
-    "Spanien": "./flag_images/spain.png",
-    "Schweiz": "./flag_images/switzerland.png",
-    "Turkiet": "./flag_images/turkey.png",
-    "USA": "./flag_images/usa.png",
-    "Wales": "./flag_images/wales.png",
+    "England": "/flag_images/england.png",
+    "Danmark": "/flag_images/denmark.png",
+    "Italien": "/flag_images/italy.png",
+    "Frankrike": "/flag_images/france.png",
+    "Argentina": "/flag_images/argentina.png",
+    "Belgien": "/flag_images/belgium.png",
+    "Bosnien": "/flag_images/bosnia.png",
+    "Brasilien": "/flag_images/brazil.png",
+    "Chile": "/flag_images/chile.png",
+    "Kina": "/flag_images/china.png",
+    "Colombia": "/flag_images/colombia.png",
+    "Kroatien": "/flag_images/croatia.png",
+    "Tjeckien": "/flag_images/czechia.png",
+    "Tyskland": "/flag_images/germany.png",
+    "Grekland": "/flag_images/grecce.png",
+    "Irland": "/flag_images/ireland.png",
+    "Israel": "/flag_images/israel.png",
+    "Mexiko": "/flag_images/mexico.png",
+    "Montenegro": "/flag_images/montenegro.png",
+    "Nederländerna": "/flag_images/netherlands.png",
+    "Paraguay": "/flag_images/paraguay.png",
+    "Portugal": "/flag_images/portugal.png",
+    "Qatar": "/flag_images/qatar.png",
+    "Ryssland": "/flag_images/russia.png",
+    "Saudiarabien": "/flag_images/saudi_arabia.png",
+    "Skottland": "/flag_images/scotland.png",
+    "Serbien": "/flag_images/serbia.png",
+    "Slovenien": "/flag_images/slovenia.png",
+    "Sydafrika": "/flag_images/south_africa.png",
+    "Spanien": "/flag_images/spain.png",
+    "Schweiz": "/flag_images/switzerland.png",
+    "Turkiet": "/flag_images/turkey.png",
+    "USA": "/flag_images/usa.png",
+    "Wales": "/flag_images/wales.png",
 };
 
 
@@ -58,8 +58,8 @@ const flagImages: FlagData = {
 
 let playerIndex = 0;
 let isAnswering = false;
-let correct = 0;
-let currentPlayerName = '';
+let points = 0;
+let currentPlayerName = "";
 let attempts = 0;
 
 
@@ -113,17 +113,16 @@ function createPlayerElement(player: Spelare): HTMLElement {
                     textContent = karriar.klubb;
                     break;
                 case 'seriematcher':
-                    textContent = `Matcher: ${karriar.seriematcher}`;
+                    textContent = `${karriar.seriematcher}`;
                     break;
                 case 'mål':
-                    textContent = `Mål: ${karriar.mål}`;
+                    textContent = `${karriar.mål}`;
                     break;
                 case 'klubbLand':
                     const flagImg = document.createElement('img');
                     flagImg.src = flagImages[karriar.klubbLand]; 
                     flagImg.alt = karriar.klubbLand; 
-                    console.log(flagImg.src);
-                    textContent = `Klubblag: `;
+                    textContent =  flagImg.alt;
                     valueElement.appendChild(flagImg);
                     break;
                     
@@ -136,7 +135,7 @@ function createPlayerElement(player: Spelare): HTMLElement {
        
         playerElement.appendChild(attrContainer);
     });
-
+  
     return playerElement;
 }
 
@@ -144,10 +143,11 @@ function showNextPlayer() {
     if (playerIndex < currentRandomPlayers.length) {
         const player = currentRandomPlayers[playerIndex];
         currentPlayerName = player.namn.trim().toLocaleLowerCase();; 
+        console.log("Current Player Name set to:", currentPlayerName);
         renderPlayer(player);
         playerIndex++;
     } else {
-       
+        console.log("Nada mas");
     }
 }
 
@@ -179,19 +179,35 @@ startBtnHard.addEventListener("click", function() {
     updateUIForGameStart()
     startGameWithPlayers(svarareSpelare.svararespelare);
     //få fram nuvarande spelare
-    const currentPlayer = currentRandomPlayers[playerIndex - 1];
+    let currentPlayer = currentRandomPlayers[playerIndex - 1];
     // Men måste få namne....
 })};
 
-if (guessBtnEL){
-    guessBtnEL.addEventListener("click", function() {
+
+
+if (guessForm && guessInputEL) {
+    guessForm.addEventListener("submit", function(e) {
+        e.preventDefault(); 
+
         const userGuess = guessInputEL.value.trim().toLocaleLowerCase();
+        console.log("User Guess:", userGuess); // Log user's guess
+        console.log("Current Player Name:", currentPlayerName);
+
         if (userGuess !== currentPlayerName) {
-            console.log("fel, hihihih")
+            console.log("fel, hihihih");
         } else {
-            console.log("rätt")
-            correct++;
+            showNextPlayer();
+            console.log("rätt");
+            points++;
             attempts++;
+            guessInputEL.value = "";
+
         }
-    })
+    });
+} else {
+    console.error("Form or input element not found");
 }
+
+
+
+
